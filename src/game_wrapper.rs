b8163680907572f971a76120dbfe5ed931718efc
Youva GAUDÉ <youvagaude@gmail.com>
2026-06-27 22:53:00 +0200

    Add game module, add ball ⚽

--- src/game_wrapper.rs
+++ src/game_wrapper.rs
@@ -1,12 +1,14 @@
+mod game;
 mod game_pause;
 
 use bevy::prelude::*;
 
 use crate::despawn_ui;
-use game_pause::IsPaused;
+pub use game_pause::IsPaused;
 
 pub fn plugin(app: &mut App) {
     app.add_plugins(game_pause::plugin)
+        .add_plugins(game::plugin)
         .add_systems(OnEnter(IsPaused::Running), game_ui.spawn())
         .add_systems(OnExit(IsPaused::Running), despawn_ui)
         .add_systems(Update, handle_input.run_if(in_state(IsPaused::Running)));
@@ -20,18 +22,18 @@ fn handle_input(input: Res<ButtonInput<KeyCode>>, mut next_state: ResMut<NextSta
 
 fn game_ui() -> impl Scene {
     bsn! {
-        Node {
-            width: percent(100),
-            height: percent(100),
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-        }
-        Children [
-            (
-                Text::new("In construction\nPress escape to quit to main menu.")
-                // TextColor(Color::BLACK)
-                TextLayout::justify(Justify::Center)
-            )
-        ]
+        // Node {
+        //     width: percent(100),
+        //     height: percent(100),
+        //     align_items: AlignItems::Center,
+        //     justify_content: JustifyContent::Center,
+        // }
+        // Children [
+        //     (
+        //         Text::new("In construction\nPress escape to quit to main menu.")
+        //         // TextColor(Color::BLACK)
+        //         TextLayout::justify(Justify::Center)
+        //     )
+        // ]
     }
 }
---
+++ src/game_wrapper/game.rs
@@ -0,0 +1,55 @@
+use bevy::prelude::*;
+
+use super::IsPaused;
+
+use crate::GameState;
+
+#[derive(Component, Clone, Default)]
+#[require(Transform)]
+struct Position(Vec2);
+
+#[derive(SceneComponent, Clone, Default)]
+struct Ball;
+
+const BALL_SIZE: f32 = 10.;
+const BALL_SHAPE: Circle = Circle::new(BALL_SIZE);
+const BALL_COLOR: Color = Color::srgb(1., 0., 0.);
+
+impl Ball {
+    fn scene() -> impl Scene {
+        let x = 0.;
+        let y = -250.;
+        bsn! {
+            Position(vec2(x,y))
+            Mesh2d(asset_value(BALL_SHAPE))
+            MeshMaterial2d<ColorMaterial>(asset_value(BALL_COLOR))
+            DespawnOnExit<GameState>(GameState::InGame)
+        }
+    }
+}
+
+pub fn plugin(app: &mut App) {
+    app.add_systems(OnEnter(GameState::InGame), entities.spawn())
+        .add_systems(
+            FixedUpdate,
+            (move_ball, project_positions)
+                .chain()
+                .run_if(in_state(IsPaused::Running)),
+        );
+}
+
+fn move_ball(mut position: Single<&mut Position, With<Ball>>) {
+    position.0.y -= 1.0;
+}
+
+fn entities() -> impl SceneList {
+    bsn_list! [
+        @Ball,
+    ]
+}
+
+fn project_positions(mut positionables: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.);
+    }
+}
