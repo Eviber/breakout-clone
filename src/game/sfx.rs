a82e7566a8f6a2ad9e2a3d27ca98932a005493ce
Youva GAUDÉ <youvagaude@gmail.com>
2026-07-09 00:10:15 +0200

    Make the beep randomized on a scale

--- src/game/sfx.rs
+++ src/game/sfx.rs
@@ -8,17 +8,36 @@ use bevy::{
 use super::ball::BallCollision;
 
 const BEEP_LENGTH_MS: u32 = 100;
-const BEEP_PITCH: f32 = 150.0;
 const BEEP_VOLUME: Volume = Volume::Linear(0.2);
+const ROOT_FREQ: f32 = 150.0;
+// TODO: Pentatonic?
+const SCALE: [i32; 7] = [0, 2, 4, 5, 7, 9, 11];
 
 pub fn plugin(app: &mut App) {
     app.add_observer(play_hit_sound);
 }
 
+/// Eheheheh 😈
+fn rand64() -> u64 {
+    use std::hash::{BuildHasher, Hasher};
+    std::hash::RandomState::new().build_hasher().finish()
+}
+
+fn random_note() -> f32 {
+    let total_notes = SCALE.len() as u64;
+
+    let idx = rand64() % total_notes;
+
+    let semitone_offset = SCALE[idx as usize];
+
+    ROOT_FREQ * 2f32.powf(semitone_offset as f32 / 12.0)
+}
+
 fn play_hit_sound(_event: On<BallCollision>, mut commands: Commands) {
+    let pitch = random_note();
     commands.spawn_scene(bsn! {
         AudioPlayer<Pitch>(asset_value(Pitch::new(
-            BEEP_PITCH,
+            pitch,
             Duration::new(0, BEEP_LENGTH_MS * 1_000_000),
         )))
         PlaybackSettings {
