63d164bd3382407bfb18bbbdb4da07cfb3fc78cc
Youva GAUDÉ <youvagaude@gmail.com>
2026-07-12 19:35:00 +0200

    Disable WorldInspector plugin

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod game;
 mod main_menu;
 
 use bevy::{feathers::FeathersPlugins, prelude::*};
-use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+// use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 #[derive(States, Default, Debug, Hash, Eq, PartialEq, Clone)]
 pub enum AppState {
@@ -19,8 +19,8 @@ pub enum AppState {
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, FeathersPlugins))
-        .add_plugins(EguiPlugin::default())
-        .add_plugins(WorldInspectorPlugin::new())
+        // .add_plugins(EguiPlugin::default())
+        // .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(main_menu::plugin)
         .add_plugins(game::plugin)
         .init_state::<AppState>()
